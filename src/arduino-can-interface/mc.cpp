d60d254920aa1f27c524182638b68b0bb3a3e4ef
Shashank Singh <sshashank124@gmail.com>
2015-09-24 19:16:53 -0400

    Added comments to code for clarity

--- src/arduino-can-interface/mc.cpp
+++ src/arduino-can-interface/mc.cpp
@@ -4,19 +4,24 @@
 
 float MC::getTemp(char* data) {
     float res;
+    // store second float (bytes 4-7) of data in res
     memcpy(&res, &data[4], sizeof(float));
     return res;
 }
 
 float MC::getRPM(char* data) {
     float res;
+    // store second float (bytes 4-7) of data in res
     memcpy(&res, &data[4], sizeof(float));
     return res;
 }
 
 float MC::getCurrent(char* data) {
     float real, imag;
+    // store first float (bytes 0-3) of data in real
     memcpy(&real, &data[0], sizeof(float));
+    // store second float (bytes 4-7) of data in imag
     memcpy(&imag, &data[4], sizeof(float));
+    // return magnitude of complex current
     return sqrt(pow(real, 2.f) + pow(imag, 2.f));
 }
