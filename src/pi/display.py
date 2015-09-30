5a94988b25e446deed1476b15e5d09d7d32f10db
Shashank Singh <sshashank124@gmail.com>
2015-09-30 19:03:48 -0400

    Updated pi bluetooth code and deleted todo.txt

--- src/pi/display.py
+++ src/pi/display.py
@@ -1,20 +1,30 @@
 # Bluetooth server code
 import bluetooth
+from struct import unpack
 
 server_addr = "A0:A8:CD:B5:52:97"
 client_addr = "2C:8A:72:ED:BE:F1"
 UUID = "d4b5ed27-e431-4c56-876e-404343f38a9b"
 port = 45067
 
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-server_sock.bind(("",port))
-server_sock.listen(1)
+def create_server_and_client_socket():
+    print "## Waiting for connection..."
+    ss = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    ss.bind((server_addr, port))
+    ss.listen(1)
+    bluetooth.advertise_service(ss, "HyTech Racing", UUID)
+    cs = ss.accept()[0]
+    print "## Client found"
+    return (ss, cs)
 
-bluetooth.advertise_service(server_sock, "HyTech Racing", UUID)
+server_sock, client_sock = create_server_and_client_socket()
 
-print "Waiting for connection..."
-client_sock = server_sock.accept()[0]
-print "Client found"
 while True:
-    data_length = int(client_sock.recv(1).encode('hex'), 16)
-    print client_sock.recv(data_length)
+    try:
+        # data_length = unpack("<i", client_sock.recv(4))[0]
+        print client_sock.recv(8)
+    except bluetooth.btcommon.BluetoothError:
+        print "## Lost Bluetooth connection"
+        server_sock.close()
+        server_sock, client_sock = create_server_and_client_socket()
+        continue
