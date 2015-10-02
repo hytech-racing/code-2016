006850b0e1c8c79bf159089a73948e34b2b78713
Shashank Singh <sshashank124@gmail.com>
2015-10-02 14:21:26 -0400

    Completed Bluetooth server (Pi) Code

--- src/pi/display.py
+++ src/pi/display.py
@@ -1,11 +1,10 @@
-# Bluetooth server code
 import bluetooth
-from struct import unpack
 
 server_addr = "A0:A8:CD:B5:52:97"
 client_addr = "2C:8A:72:ED:BE:F1"
-UUID = "d4b5ed27-e431-4c56-876e-404343f38a9b"
-port = 45067
+UUID = "0000110a-0000-1000-8000-00805f9b34fb"
+port = 1
+DATA_LENGTH = 20
 
 def create_server_and_client_socket():
     print "## Waiting for connection..."
@@ -21,10 +20,8 @@ server_sock, client_sock = create_server_and_client_socket()
 
 while True:
     try:
-        # data_length = unpack("<i", client_sock.recv(4))[0]
-        print client_sock.recv(8)
+        print client_sock.recv(DATA_LENGTH)
     except bluetooth.btcommon.BluetoothError:
         print "## Lost Bluetooth connection"
         server_sock.close()
         server_sock, client_sock = create_server_and_client_socket()
-        continue
