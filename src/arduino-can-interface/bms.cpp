f1c3028af5ed3fe6a9ca459ffefc3426ead5efda
Ryan Gallaway <rgallaway@gatech.edu>
2015-10-08 18:33:21 -0400

    updated bms functions

--- src/arduino-can-interface/bms.cpp
+++ src/arduino-can-interface/bms.cpp
@@ -1,6 +1,6 @@
 #include "bms.h"
 
-float BMS::getVoltage(char[] data) {
+float BMS::getVoltage(unsigned char* data) {
     unsigned char high = data[2];
     unsigned char low = data[3];
 
@@ -8,10 +8,17 @@ float BMS::getVoltage(char[] data) {
     return volts;
 }
 
-float BMS::getCurrent(char[] data) {
+float BMS::getCurrent(unsigned char* data) {
+    unsigned char high = data[0];
+    unsigned char low = data[1];
 
+    float current = (high << 8) | low;
+    return current;
 }
 
-float BMS::getTemp(char[] data) {
+float BMS::getTemp(unsigned char* data) {
+    unsigned char uTemp = data[4]; //Replace with actual value when we know what it is
+    byte temp = (byte) uTemp;
 
+    return temp;
 }
