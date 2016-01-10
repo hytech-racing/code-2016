c0eee0abedc3f46894da3912cfce6ade3e50eb44
Ryan Gallaway <rgallaway@gatech.edu>
2016-01-10 16:32:03 -0500

    updated BMS header file to reflect list from gdocs

--- src/arduino-can-interface/bms.h
+++ src/arduino-can-interface/bms.h
@@ -16,14 +16,48 @@ namespace BMS {
 	int Message_5 = 0x05; // low cell voltage
 	int Message_6 = 0x06; // open cell voltages-  probably dont need
 	int Message_7 = 0x07; // cell resistances- probably dont need
-	
-    float getPackVoltage(char*);
-    float getCurrent(char*);
-    float getHighTemp(char*);
+
+	// 0x001: Pack Information
+	float getRelayStatus(char*);
+	float getPopulatedCellCount(char*);
 	float getStateOfCharge(char*);
-	float getHighResistance(char*);
+	float getDepthOfDischarge(char*);
+	float getPackHealth(char*);
+	float getPackCycles(char*);
+	float getSupplyVoltage(char*);
+
+	// 0x002: Pack Values 1
+	float getPackCCL(char*);
+    float getCurrent(char*);
+    float getPackDCL(char*);
+    float getAmpHours(char*);
+
+    // 0x003: Pack Values 2
+    float getPackVoltage(char*);
+    float getPackOpenVoltage(char*);
+    float getPackSumVoltage(char*);
+    float getPackResistance(char*);
+
+    // 0x004: Temperature Values
+    float getHighTemp(char*);
+    float getLowTemp(char*);
+    float getAvgTemp(char*);
+    float getInternalTemp(char*);
+
+    // 0x005: Cell Voltages
+    float getHighVoltage(char*);
 	float getLowVoltage(char*);
-	float getPowerDischargeLimit(char*);
+	float getAvgVoltage(char*);
+
+	// 0x006: Open Cell Voltages
+	float getHighOpenVoltage(char*);
+	float getLowOpenVoltage(char*);
+	float getAvgOpenVoltage(char*);
+
+	// 0x007: Cell Resistances
+	float getHighResistance(char*);
+	float getLowResistance(char*);
+	float getAvgResistance(char*);
 }
 /*
 float BMSstateOfCharge;
