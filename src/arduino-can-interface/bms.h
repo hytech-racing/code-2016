c4335a5b13a1571e896dc876c4a1184f972fd794
Ryan Gallaway <rgallaway@gatech.edu>
2016-01-10 16:33:34 -0500

    updated BMS header file to USE SPACES INSTEAD OF TABS

--- src/arduino-can-interface/bms.h
+++ src/arduino-can-interface/bms.h
@@ -9,25 +9,25 @@
  */
 
 namespace BMS {
-	int Message_1 = 0x01; // state of charge and pack health
-	int Message_2 = 0x02; // current, charge and discharge current limits
-	int Message_3 = 0x03; // pack voltage and open voltage
-	int Message_4 = 0x04; // high temperature
-	int Message_5 = 0x05; // low cell voltage
-	int Message_6 = 0x06; // open cell voltages-  probably dont need
-	int Message_7 = 0x07; // cell resistances- probably dont need
+    int Message_1 = 0x01; // state of charge and pack health
+    int Message_2 = 0x02; // current, charge and discharge current limits
+    int Message_3 = 0x03; // pack voltage and open voltage
+    int Message_4 = 0x04; // high temperature
+    int Message_5 = 0x05; // low cell voltage
+    int Message_6 = 0x06; // open cell voltages-  probably dont need
+    int Message_7 = 0x07; // cell resistances- probably dont need
 
-	// 0x001: Pack Information
-	float getRelayStatus(char*);
-	float getPopulatedCellCount(char*);
-	float getStateOfCharge(char*);
-	float getDepthOfDischarge(char*);
-	float getPackHealth(char*);
-	float getPackCycles(char*);
-	float getSupplyVoltage(char*);
+    // 0x001: Pack Information
+    float getRelayStatus(char*);
+    float getPopulatedCellCount(char*);
+    float getStateOfCharge(char*);
+    float getDepthOfDischarge(char*);
+    float getPackHealth(char*);
+    float getPackCycles(char*);
+    float getSupplyVoltage(char*);
 
-	// 0x002: Pack Values 1
-	float getPackCCL(char*);
+    // 0x002: Pack Values 1
+    float getPackCCL(char*);
     float getCurrent(char*);
     float getPackDCL(char*);
     float getAmpHours(char*);
@@ -46,18 +46,18 @@ namespace BMS {
 
     // 0x005: Cell Voltages
     float getHighVoltage(char*);
-	float getLowVoltage(char*);
-	float getAvgVoltage(char*);
+    float getLowVoltage(char*);
+    float getAvgVoltage(char*);
 
-	// 0x006: Open Cell Voltages
-	float getHighOpenVoltage(char*);
-	float getLowOpenVoltage(char*);
-	float getAvgOpenVoltage(char*);
+    // 0x006: Open Cell Voltages
+    float getHighOpenVoltage(char*);
+    float getLowOpenVoltage(char*);
+    float getAvgOpenVoltage(char*);
 
-	// 0x007: Cell Resistances
-	float getHighResistance(char*);
-	float getLowResistance(char*);
-	float getAvgResistance(char*);
+    // 0x007: Cell Resistances
+    float getHighResistance(char*);
+    float getLowResistance(char*);
+    float getAvgResistance(char*);
 }
 /*
 float BMSstateOfCharge;
