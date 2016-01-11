eecdd1ca4db9be47789a521aaa9d42217c5585bb
Shashank Singh <sshashank124@gmail.com>
2016-01-10 21:43:21 -0500

    Finished src/arduino-can-interface/mc.h and mc.cpp

--- src/arduino-can-interface/mc.cpp
+++ src/arduino-can-interface/mc.cpp
@@ -1,7 +1,8 @@
 #include "mc.h"
 #include <cstring>
 
-float MC::getPhaseTemp(char *data) {
+// 0x0A0
+float MC::getAveragePhaseTemp(unsigned char *data) {
     short phaseA, phaseB, phaseC;
     memcpy(&phaseA, &data[0], sizeof(short));
     memcpy(&phaseB, &data[2], sizeof(short));
@@ -9,20 +10,144 @@ float MC::getPhaseTemp(char *data) {
     return (phaseA + phaseB + phaseC) / 30.0f;
 }
 
-float MC::getMotorTemp(char *data) {
+float MC::getGateDriverTemp(unsigned char *data) {
+    short gateDriverTemp;
+    memcpy(&gateDriverTemp, &data[6], sizeof(short));
+    return gateDriverTemp / 10.0f;
+}
+
+// 0x0A1
+float MC::getControlBoardTemp(unsigned char *data) {
+    short boardTemp;
+    memcpy(&boardTemp, &data[0], sizeof(short));
+    return boardTemp / 10.0f;
+}
+
+float MC::getThermistor1Temp(unsigned char *data) {
+    short t1Temp;
+    memcpy(&t1Temp, &data[2], sizeof(short));
+    return t1Temp / 10.0f;
+}
+
+float MC::getThermistor2Temp(unsigned char *data) {
+    short t2Temp;
+    memcpy(&t2Temp, &data[4], sizeof(short));
+    return t2Temp / 10.0f;
+}
+
+// 0x0A2
+float MC::getMotorTemperature(unsigned char *data) {
     short motorTemp;
     memcpy(&motorTemp, &data[4], sizeof(short));
     return motorTemp / 10.0f;
 }
 
-float MC::getRPM(char *data) {
-    short motorSpeed;
-    memcpy(&motorSpeed, &data[2], sizeof(short));
-    return (float) motorSpeed;
+// 0x0A4
+bool MC::isForwardSwitchOn(unsigned char *data) {
+    return data[0];
 }
 
-float MC::getCurrent(char *data) {
-    short current;
-    memcpy(&current, &data[6], sizeof(short));
-    return current / 10.0f;
+bool MC::isReverseSwitchOn(unsigned char *data) {
+    return data[1];
+}
+
+bool MC::isBrakeSwitchOn(unsigned char *data) {
+    return data[2];
+}
+
+bool MC::isRegenOn(unsigned char *data) {
+    return !((bool) data[3]);
+}
+
+bool MC::isIgnitionOn(unsigned char *data) {
+    return data[4];
+}
+bool MC::isStartSwitchOn(unsigned char *data) {
+    return data[5];
+}
+
+// 0x0A5
+int MC::getMotorRPM(unsigned char *data) {
+    short motorRPM;
+    memcpy(&motorRPM, &data[2], sizeof(short));
+    return motorRPM;
+}
+
+// 0x0A6
+float MC::getPhaseACurrent(unsigned char *data) {
+    short aI;
+    memcpy(&aI, &data[0], sizeof(short));
+    return aI / 10.0f;
+}
+
+float MC::getPhaseBCurrent(unsigned char *data) {
+    short bI;
+    memcpy(&bI, &data[2], sizeof(short));
+    return bI / 10.0f;
+}
+
+float MC::getPhaseCCurrent(unsigned char *data) {
+    short cI;
+    memcpy(&cI, &data[4], sizeof(short));
+    return cI / 10.0f;
+}
+
+float MC::getDCBusCurrent(unsigned char *data) {
+    short dcI;
+    memcpy(&dcI, &data[6], sizeof(short));
+    return dcI / 10.0f;
+}
+
+// 0x0A7
+float MC::getDCBusVoltage(unsigned char *data) {
+    short dcV;
+    memcpy(&dcV, &data[0], sizeof(short));
+    return dcV / 10.0f;
+}
+
+float MC::getOutputVoltage(unsigned char *data) {
+    short oV;
+    memcpy(&oV, &data[2], sizeof(short));
+    return oV / 10.0f;
+}
+
+float MC::getPhaseABVoltage(unsigned char *data) {
+    short abV;
+    memcpy(&abV, &data[4], sizeof(short));
+    return abV / 10.0f;
+}
+
+float MC::getPhaseBCVoltage(unsigned char *data) {
+    short bcV;
+    memcpy(&bcV, &data[6], sizeof(short));
+    return bcV / 10.0f;
+}
+
+// 0x0AA
+unsigned char MC::getInverterState(unsigned char *data) {
+    return data[2];
+}
+
+bool MC::isInverterOn(unsigned char *data) {
+    return data[6] & 0x1;
+}
+
+// 0x0AB
+unsigned long MC::getPOSTFault(unsigned char *data) {
+    unsigned long errorCode;
+    memcpy(&errorCode, &data[0], sizeof(unsigned long));
+    return errorCode;
+}
+
+unsigned long MC::getRUNFault(unsigned char *data) {
+    unsigned long errorCode;
+    memcpy(&errorCode, &data[4], sizeof(unsigned long));
+    return errorCode;
+}
+
+// 0x0AC
+unsigned long MC::getElapsedTime(unsigned char *data) {
+    unsigned long elapsedTime;
+    memcpy(&elapsedTime, &data[4], sizeof(unsigned long));
+    return (unsigned long) (elapsedTime * 0.003f);
 }
--- src/arduino-can-interface/mc.h
+++ src/arduino-can-interface/mc.h
@@ -7,9 +7,7 @@
 
 namespace MC {
     // 0x0A0
-    float getModuleATemp(unsigned char *);
-    float getModuleBTemp(unsigned char *);
-    float getModuleCTemp(unsigned char *);
+    float getAveragePhaseTemp(unsigned char *);
     float getGateDriverTemp(unsigned char *);
     // 0x0A1
     float getControlBoardTemp(unsigned char *);
@@ -19,14 +17,14 @@ namespace MC {
     // 0x0A2
     float getMotorTemperature(unsigned char *);
     // 0x0A4
-    float isForwardSwitchEnabled(unsigned char *);
-    float isReverseSwitchEnabled(unsigned char *);
-    float isBrakeSwitchEnabled(unsigned char *);
-    float isRegenEnabled(unsigned char *);
-    float isIgnitionEnabled(unsigned char *);
-    float isStartSwitchEnabled(unsigned char *);
+    bool isForwardSwitchOn(unsigned char *);
+    bool isReverseSwitchOn(unsigned char *);
+    bool isBrakeSwitchOn(unsigned char *);
+    bool isRegenOn(unsigned char *);
+    bool isIgnitionOn(unsigned char *);
+    bool isStartSwitchOn(unsigned char *);
     // 0x0A5
-    float getMotorSpeed(unsigned char *);
+    int getMotorRPM(unsigned char *);
     // 0x0A6
     float getPhaseACurrent(unsigned char *);
     float getPhaseBCurrent(unsigned char *);
@@ -37,16 +35,19 @@ namespace MC {
     float getOutputVoltage(unsigned char *);
     float getPhaseABVoltage(unsigned char *);
     float getPhaseBCVoltage(unsigned char *);
-    // 0x0A9
-    float get5ReferenceVoltage(unsigned char *);
-    float get12ReferenceVoltage(unsigned char *);
     // 0x0AA
-    float STATE_POWER_ON = 0;
-    float STATE_STOP = 1;
-    float STATE_OPEN_LOOP = 2;
-    float STATE_CLOSED_LOOP = 3;
-    float STATE_WAIT = 4;
-    float STATE_IDLE_RUN = 8;
-    float STATE_IDLE_STOP = 9;
-    float getInverterState(unsigned char *);
+    unsigned char STATE_POWER_ON = 0;
+    unsigned char STATE_STOP = 1;
+    unsigned char STATE_OPEN_LOOP = 2;
+    unsigned char STATE_CLOSED_LOOP = 3;
+    unsigned char STATE_WAIT = 4;
+    unsigned char STATE_IDLE_RUN = 8;
+    unsigned char STATE_IDLE_STOP = 9;
+    unsigned char getInverterState(unsigned char *);
+    bool isInverterOn(unsigned char *);
+    // 0x0AB
+    unsigned long getPOSTFault(unsigned char *);
+    unsigned long getRUNFault(unsigned char *);
+    // 0x0AC
+    unsigned long getElapsedTime(unsigned char *);
 }
