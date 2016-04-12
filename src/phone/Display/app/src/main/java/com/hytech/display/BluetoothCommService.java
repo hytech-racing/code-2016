daffbdfe55f785ce18ed4b809c28903ebf21367c
Shashank Singh <sshashank124@gmail.com>
2016-04-12 19:13:24 -0400

    Holy shit I fucked up

--- src/phone/Display/app/src/main/java/com/hytech/display/BluetoothCommService.java
+++ src/phone/Display/app/src/main/java/com/hytech/display/BluetoothCommService.java
@@ -1,6 +1,5 @@
 package com.hytech.display;
 
-import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -21,7 +20,7 @@ public class BluetoothCommService {
     private SetupConnection btSetupConnection;
     private CommunicationThread btCommunicationThread;
 
-    private static final String LOG_TAG = "RETRYING";
+    private static final String LOG_TAG = "BT_COMM";
     // Laptop Address
     // private static final String SERVER_ADDR = "A0:A8:CD:B5:52:97";
     // RPi Address
@@ -37,14 +36,28 @@ public class BluetoothCommService {
     }
 
     public synchronized void start() {
+        if (btCommunicationThread != null) {
+            btCommunicationThread.interrupt();
+            btCommunicationThread = null;
+        }
         btSetupConnection = new SetupConnection();
+//        log("Connecting");
         btSetupConnection.start();
     }
 
     public synchronized void connected() {
-        btSetupConnection = null;
+        if (btSetupConnection != null) {
+            btSetupConnection.interrupt();
+            btSetupConnection = null;
+        }
         btCommunicationThread = new CommunicationThread();
+//        log("Talking");
         btCommunicationThread.start();
+    }
+
+    public synchronized void disconnected() {
+//        log("Disconnected");
+        start();
     }
 
     private class SetupConnection extends Thread {
@@ -52,33 +65,37 @@ public class BluetoothCommService {
         public void run() {
             if (!btAdapter.isEnabled()) btAdapter.enable();
             while (!btAdapter.isEnabled());
-            log("Bluetooth enabled");
             BluetoothDevice btServer = btAdapter.getRemoteDevice(SERVER_ADDR);
             btAdapter.cancelDiscovery();
-            try {
-                btSocket = btServer.createInsecureRfcommSocketToServiceRecord(
-                        UUID.fromString(B_UUID));
-            } catch (Exception e) {
-                log("Socket creation failed");
-                return;
-            }
-            try {
-                btSocket.connect();
-            } catch (IOException e) {
-                log("Socket connection method #1 failed");
+            while (btSocket == null || !btSocket.isConnected()) {
                 try {
-                    btSocket = (BluetoothSocket) btServer.getClass()
-                            .getMethod("createRfcommSocket",
-                                    new Class[] {int.class}).invoke(btServer,
-                                    PORT);
-                    btSocket.connect();
+                    btSocket = btServer
+                            .createInsecureRfcommSocketToServiceRecord(
+                            UUID.fromString(B_UUID));
+                } catch (Exception e) {
+                    continue;
                 }
-                catch (Exception e2) {
-                    log("Socket connection method #2 failed");
-                    return;
+                try {
+                    btSocket.connect();
+                } catch (IOException e) {
+                    try {
+                        btSocket = (BluetoothSocket) btServer.getClass()
+                                .getMethod("createRfcommSocket",
+                                        new Class[] {int.class})
+                                .invoke(btServer, PORT);
+                        btSocket.connect();
+                    }
+                    catch (Exception e2) {
+//                        log("Connection Failed");
+                        try {
+                            btSocket.close();
+                        } catch (IOException e3) {
+//                            log("Couldn't close socket");
+                        }
+                    }
                 }
             }
-            log("Socket created and connected");
+//            log("Socket created and connected");
             connected();
         }
     }
@@ -94,46 +111,50 @@ public class BluetoothCommService {
                 tmpIn = btSocket.getInputStream();
                 tmpOut = btSocket.getOutputStream();
             } catch (IOException e) {
-                log("IO streams creation failed");
+//                log("IO streams creation failed");
             }
             btReader = tmpIn;
             btWriter = tmpOut;
-            log("IO streams created");
+//            log("IO streams created");
         }
 
         @Override
         public void run() {
             byte[] buffer = new byte[DATA_LENGTH];
             int bytes;
-            while (true) {
+            boolean running = true;
+            while (running) {
                 try {
                     bytes = btReader.read(buffer);
-                    btHandler.obtainMessage(0, bytes, 0, buffer)
-                            .sendToTarget();
+                    btHandler.obtainMessage(0, bytes, 0, buffer).sendToTarget();
                 } catch (IOException e) {
-                    log("disconnected? " + e.getMessage());
-                    break;
+                    buffer[0] = MainActivity.BT_DISCONNECT;
+                    btHandler.obtainMessage(0, 1, 0, buffer).sendToTarget();
+                    try {
+                        btReader.close();
+                        btWriter.close();
+                    } catch (IOException e1) {
+                        log("Couldn't close streams");
+                    }
+                    running = false;
                 }
             }
-            buffer[0] = 0x7F;
-            btHandler.obtainMessage(0, 1, 0, buffer)
-                    .sendToTarget();
+            disconnected();
         }
 
         public void write(byte[] buffer) {
             try {
                 btWriter.write(buffer);
-                btHandler.obtainMessage(0, 0, 0, buffer)
-                        .sendToTarget();
-            } catch (IOException e) {
-            }
+            } catch (IOException e) { }
         }
     }
 
     public void write(byte[] out) {
-        CommunicationThread r;
-        r = btCommunicationThread;
-        r.write(out);
+        if (btCommunicationThread != null) {
+            btCommunicationThread.write(out);
+        } else {
+            log("BT_COMM_THREAD is null");
+        }
     }
 
     public static void log(String message) {
--- src/phone/Display/app/src/main/java/com/hytech/display/MainActivity.java
+++ src/phone/Display/app/src/main/java/com/hytech/display/MainActivity.java
@@ -1,127 +1,423 @@
 package com.hytech.display;
 
+
+
 import android.app.Activity;
+
 import android.app.AlertDialog;
-import android.content.DialogInterface;
+
 import android.hardware.Sensor;
+
 import android.hardware.SensorEvent;
+
 import android.hardware.SensorEventListener;
+
 import android.hardware.SensorManager;
+
 import android.os.Bundle;
+
 import android.os.Handler;
+
 import android.os.Message;
-import android.util.Log;
+
 import android.view.Window;
+
 import android.view.WindowManager;
+
 import android.widget.TextView;
 
-import com.github.pavlospt.CircleView;
+
+
+import com.github.lzyzsd.circleprogress.ArcProgress;
+
 
 
 public class MainActivity extends Activity implements SensorEventListener {
 
-    private CircleView odometer;
-    private TextView stateOfCharge;
-    private TextView batteryTemps;
+
+
+    // region #CONSTANTS
+
+    public int GREEN;
+
+    public int YELLOW;
+
+    public int ORANGE;
+
+    public int RED;
+
+    public int[] PROGRESS_COLORS;
+
+
+
+    public static final byte BT_BATT_SOC = 1;
+
+    public static final byte BT_BATT_TIME_LEFT = 2;
+
+    public static final byte BT_BATT_HI_AVG_TEMP = 4;
+
+    public static final byte BT_CAR_STARTUP_STATE = 6;
+
+    public static final byte BT_MOTOR_TEMP = 8;
+
+    public static final byte BT_CAR_SPEED = 10;
+
+    public static final byte BT_DISCONNECT = 0x7F;
+
+    // endregion
+
+
+
+    private ArcProgress chargeMeter;
+
+    private int currentProgressColor = RED;
+
+
+
+    private TextView speedView;
+
+    private TextView batterySOC;
+
+    private TextView battTemps;
+
+
 
     private SensorManager sensorManager;
+
     private Sensor accelerometer;
+
+
 
     private float previousRotation = 0;
 
+
+
+    private AlertDialog infoDisplay;
+
+    private byte currentState = -1;
+
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_main);
 
+
+
+        // region #SETUP COLORS
+
+        GREEN = getResources().getColor(R.color.green);
+
+        YELLOW = getResources().getColor(R.color.yellow);
+
+        ORANGE = getResources().getColor(R.color.orange);
+
+        RED = getResources().getColor(R.color.red);
+
+        PROGRESS_COLORS = new int[] {RED, ORANGE, YELLOW, GREEN};
+
+        // endregion
+
+
+
         BluetoothCommService bcs = new BluetoothCommService(handler);
+
         bcs.start();
 
+
+
         initSensors();
-        odometer = (CircleView) findViewById(R.id.odometer);
-        stateOfCharge = (TextView) findViewById(R.id.button1);
-        batteryTemps = (TextView) findViewById(R.id.button3);
+
+        chargeMeter = (ArcProgress) findViewById(R.id.charge_meter);
+
+        speedView = (TextView) findViewById(R.id.speed_view);
+
+        batterySOC = (TextView) findViewById(R.id.batt_soc);
+
+        battTemps = (TextView) findViewById(R.id.batt_temp);
+
+
+
+        initDefaultUI();
+
     }
+
+
+
+    // region [ BT DATA HANDLER ]
 
     private final Handler handler = new Handler(new Handler.Callback() {
+
         @Override
+
         public boolean handleMessage(Message message) {
+
             byte[] data = (byte[]) message.obj;
+
             switch (data[0]) {
-                case 1:
-                    int soc = data[1] & 0xFF;
-                    String sLabel = "Battery: " + soc + "%";
-                    stateOfCharge.setText(sLabel);
+
+                case BT_BATT_SOC:
+
+                    int soc = data[BT_BATT_SOC] & 0xFF;
+
+                    chargeMeter.setProgress(soc);
+
+                    chargeMeter.setBottomText(soc + "\uFE6A");
+
+                    int newColor = PROGRESS_COLORS[Math.min(3, soc / 25)];
+
+                    if (newColor != currentProgressColor) {
+
+                        chargeMeter.setFinishedStrokeColor(newColor);
+
+                        chargeMeter.setTextColor(newColor);
+
+                    }
+
                     break;
-                case 4:
-                    int high = data[4] & 0xFF;
-                    int avg = data[5] & 0xFF;
-                    String bTempLabel = "Battery Temps:\n Avg: " + avg + "° C\n High: " + high
-                            + "° C";
-                    batteryTemps.setText(bTempLabel);
+
+                case BT_BATT_HI_AVG_TEMP:
+
+                    int high = data[BT_BATT_HI_AVG_TEMP] & 0xFF;
+
+                    int avg = data[BT_BATT_HI_AVG_TEMP + 1] & 0xFF;
+
+                    String bTempLabel = "Battery Temps:\nAvg: " + avg
+
+                            + " °C\nHigh: " + high + " °C";
+
+                    battTemps.setText(bTempLabel);
+
                     break;
-                case 10:
-                    int speed = ((data[11] & 0xFF) << 8) | (data[10] & 0xFF);
-                    odometer.setTitleText("" + speed);
+
+                case BT_CAR_STARTUP_STATE:
+
+                    BluetoothCommService.log(currentState + ", " + data[1]);
+
+                    handleStartupCode(data[BT_CAR_STARTUP_STATE]);
                     break;
-                case 0x7F:
-                    btErrorDialog();
+
+                case BT_CAR_SPEED:
+
+                    int speed = ((data[BT_CAR_SPEED + 1] & 0xFF) << 8)
+
+                            | (data[BT_CAR_SPEED] & 0xFF);
+
+                    speedView.setText("" + speed);
+
+                    break;
+
+                case BT_DISCONNECT:
+
+                    initDefaultUI();
+
             }
-            return false;
+
+            return true;
+
         }
+
     });
 
-    public void btErrorDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setMessage(R.string.bt_err_msg).setTitle(R.string.bt_error);
-        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        AlertDialog error = builder.create();
-        error.show();
+    // endregion
+
+
+
+    public void handleStartupCode(byte startupCode) {
+
+        if (currentState == startupCode) {
+
+            return;
+
+        }
+
+        currentState = startupCode;
+
+
+
+        String message = null;
+
+        switch (startupCode) {
+
+            case 0:
+
+                message = "Dude, ur in a dope-ass car. This thing lit";
+
+                break;
+
+            case 1:
+
+                message = "Press progress button to begin startup sequence";
+
+                break;
+
+            case 2:
+
+                message = "Testing for BMS Error and loading food supply...";
+
+                break;
+
+            case 3:
+
+                message = "Press brake pedal and progress button to start car";
+
+                break;
+
+            case 4:
+
+                message = "Pre-charging capacitors and crossing fingers...";
+
+                break;
+
+            case 5:
+
+                if (infoDisplay.isShowing()) {
+
+                    infoDisplay.dismiss();
+
+                }
+
+                break;
+
+            case 8:
+
+                message = "Shutdown Complete! You did fine I guess";
+
+                break;
+
+        }
+
+
+
+        if (message != null) {
+
+            btErrorDialog(message);
+
+        }
+
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float angle = (float) Math.toDegrees(Math.atan2(-y, x));
-            float viewRotation = odometer.getRotation();
-            viewRotation += 0.5 * (angle - viewRotation);
-            if (Math.abs(viewRotation - previousRotation) > 0.2) {
-                odometer.setRotation(viewRotation);
-            }
-            previousRotation = viewRotation;
+
+
+    public void btErrorDialog(String message) {
+
+        if (infoDisplay != null && infoDisplay.isShowing()) {
+
+            infoDisplay.dismiss();
+
         }
+
+        infoDisplay = new AlertDialog.Builder(MainActivity.this)
+
+            .setMessage(message).setIcon(R.drawable.ic_launcher)
+
+                .setCancelable(false).create();
+
+        infoDisplay.show();
+
     }
+
+
+
+    private void initDefaultUI() {
+
+        chargeMeter.setProgress(0);
+
+        chargeMeter.setBottomText("---");
+
+        chargeMeter.setTextColor(RED);
+
+        chargeMeter.setFinishedStrokeColor(RED);
+
+        speedView.setText("---");
+
+    }
+
+
+
+    @Override
+
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            float x = event.values[0];
+
+            float y = event.values[1];
+
+            float angle = (float) Math.toDegrees(Math.atan2(-y, x));
+
+            float viewRotation = speedView.getRotation();
+
+            viewRotation += 0.5 * (angle - viewRotation);
+
+            if (Math.abs(viewRotation - previousRotation) > 0.2) {
+
+                speedView.setRotation(viewRotation);
+
+            }
+
+            previousRotation = viewRotation;
+
+        }
+
+    }
+
+
 
     private void initSensors() {
+
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
         accelerometer = sensorManager.getDefaultSensor(
+
                 Sensor.TYPE_ACCELEROMETER);
+
     }
 
+
+
     @Override
+
     public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, accelerometer,
-                SensorManager.SENSOR_DELAY_FASTEST);
-    }
+
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        sensorManager.unregisterListener(this, accelerometer);
+
+    public void onResume() {
+
+        super.onResume();
+
+        sensorManager.registerListener(this, accelerometer,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+
     }
+
+
+
+    @Override
+
+    public void onDestroy() {
+
+        super.onDestroy();
+
+        sensorManager.unregisterListener(this, accelerometer);
+
+    }
+
 }
+
